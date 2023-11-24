fec89cd70eeadbf067517f6fc8dbc01751bec755
Tatu Argillander <tatu.argillander@kouralabs.com>
2023-11-24 08:57:18 +0200

    nvim: Disable treesitter on large lua buffers
    Path of Building has some generated lua files I need to open and some of
    these are +10k lines. My elderly macbook completely chokes on these.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -42,8 +42,11 @@ require('nvim-treesitter.configs').setup {
     -- `false` will disable the whole extension
     enable = true,
 
-    -- list of language that will be disabled
-    disable = {},
+    -- Disable in large lua buffers. These files are killing my old mac.
+    disable = function(lang, bufnr)
+      return lang == "lua" and vim.api.nvim_buf_line_count(bufnr) > 2500
+    end,
+
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
