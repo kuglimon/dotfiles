71f0d094a3616e1eb3fec812c66d89484d740bdf
Tatu Argillander <taatelio@gmail.com>
2024-04-25 23:01:12 +0300

    nixos: make zsh the default shell
    Oh god finally, I have my dotfiles working. Been editing away on a plain
    ass environment for two days. I can't even remember what ice cream
    tastes like anymore.

--- configuration.nix
+++ configuration.nix
@@ -116,6 +116,9 @@
     neovim
   ];
 
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
