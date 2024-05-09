a979ad2a3e6d65b84809725a0f89abf1d929d4a0
Tatu Argillander <taatelio@gmail.com>
2024-05-09 14:48:36 +0300

    nixos: configure and install gnupg

--- configuration.nix
+++ configuration.nix
@@ -325,6 +325,14 @@
     };
   };
 
+  # If GPG complains about missing pinentry then try rebooting the machine.
+  # There's likely some configuration error, too lazy to debug it now.
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
