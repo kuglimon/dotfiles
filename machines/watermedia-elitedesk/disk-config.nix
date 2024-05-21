fb149cc5c71440682837f3daa86e8b95fd0f57f6
Tatu Argillander <taatelio@gmail.com>
2024-05-21 22:06:52 +0300

    watermedia: add disko configuration for partitions
    Differs from my other layouts where I have nested subvolumes. Nested
    subvolumes are a nightmare for restoring as you need to reparent
    snapshots after restore.
    
    In the future I should connect this with the flake configuration.

---
+++ machines/watermedia-elitedesk/disk-config.nix
@@ -0,0 +1,48 @@
+{
+  disko.devices = {
+    disk = {
+      vdb = {
+        type = "disk";
+        device =  "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "1024M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                subvolumes = {
+                  "@root" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@home" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/home";
+                  };
+                  "@nix-store" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/nix";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
