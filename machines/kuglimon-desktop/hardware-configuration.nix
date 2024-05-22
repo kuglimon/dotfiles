3f0c55e5130c55e5d9951ae9fff94333ff2a2de0
Tatu Argillander <taatelio@gmail.com>
2024-05-22 21:43:00 +0300

    desktop: improve btrfs subvolume layout
    This should make it easier to backup stuff through snapshots. I really
    only care about the home directory, everything else I can recreate
    through Nix.
    
    And even large parts of the home directory is already managed through
    Nix.
    
    Steam directory is rather large and should always live in it's own
    subvolume. There's no need to backup it.
    
    Individual subvolumes are used instead of nested subvolumes. Nested
    subvolumes are hard when you need to restore snapshots. From what I've
    understood I would have to rollback to a snapshot and then reparent that
    snapshot and likely all previous snapshots as well.

--- machines/kuglimon-desktop/hardware-configuration.nix
+++ machines/kuglimon-desktop/hardware-configuration.nix
@@ -19,6 +19,24 @@
       options = [ "subvol=@nixos-root" "noatime" "ssd" "compress=zstd" ];
     };
 
+  fileSystems."/home" =
+    { device = "/dev/nvme1n1p1";
+      fsType = "btrfs";
+      options = [ "subvol=@home" "noatime" "ssd" "compress=zstd" ];
+    };
+
+  fileSystems."/home/kuglimon/.local/share/Steam" =
+    { device = "/dev/nvme1n1p1";
+      fsType = "btrfs";
+      options = [ "subvol=@steam" "noatime" "ssd" "compress=zstd" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/nvme1n1p1";
+      fsType = "btrfs";
+      options = [ "subvol=@nix-store" "noatime" "ssd" "compress=zstd" ];
+    };
+
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/A836-5A29";
       fsType = "vfat";
