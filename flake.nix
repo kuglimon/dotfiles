1eced6b625152c33401b589578847711f78ec01f
Tatu Argillander <taatelio@gmail.com>
2024-08-26 07:26:18 +0300

    chore: update all systems

--- flake.nix
+++ flake.nix
@@ -32,6 +32,7 @@
     rec {
       cosmocc = legacyPackages.callPackage ./pkgs/cosmocc.nix { };
       llamafile = legacyPackages.callPackage ./pkgs/llamafile.nix { cosmocc = cosmocc; };
+      battlenet = legacyPackages.callPackage ./pkgs/battlenet-installer.nix { };
     };
 
     packages."x86_64-darwin" = let
