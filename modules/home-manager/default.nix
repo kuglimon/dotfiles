88f01b1176808c3ae856697e6074b1a79d8f9f51
Tatu Argillander <taatelio@gmail.com>
2024-10-13 17:53:30 +0300

    nix: add a note about home-manager
    I'm not even sure if I need this at all.

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,4 +1,7 @@
 { inputs, ... }: {
+  # TODO(tatu): maybe is should just drop home-manager completely. I use it for
+  # like firefox profiles and that's it. There's probably another way to install
+  # dotfiles without it.
   imports = with inputs; [
     home-manager.nixosModules.home-manager
   ];
