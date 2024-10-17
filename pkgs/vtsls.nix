4865727c238bebac69abf851831eb381a0ef0814
Tatu Argillander <taatelio@gmail.com>
2024-10-17 07:59:09 +0300

    lsp: update vtsls package
    Some changes made during the nixpkgs review. Doesn't seem to be going
    anywhere, might as well revert to updating this.

--- pkgs/vtsls.nix
+++ pkgs/vtsls.nix
@@ -4,18 +4,18 @@
   fetchFromGitHub,
   nodejs_20,
   git,
-  pnpm_8
+  pnpm_8,
 }:
 
-stdenv.mkDerivation (finalAttrs: rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "vtsls";
   version = "0.2.6";
 
   src = fetchFromGitHub {
     owner = "yioneko";
     repo = "vtsls";
-    rev = "server-v${version}";
-    sha256 = "sha256-HCi9WLh4IEfhgkQNUVk6IGkQfYagg805Rix78zG6xt0=";
+    rev = "server-v${finalAttrs.version}";
+    hash = "sha256-HCi9WLh4IEfhgkQNUVk6IGkQfYagg805Rix78zG6xt0=";
     fetchSubmodules = true;
   };
 
@@ -39,8 +39,7 @@ stdenv.mkDerivation (finalAttrs: rec {
     hash = "sha256-4XxQ0Z2atTBItrD9iY7q5rJaCmb1EeDBvQ5+L3ceRXI=";
   };
 
-  # gets around 'git submodule status' usage during build, which didn't work on
-  # stdenv for some reason.
+  # Patches to get submodule sha from file instead of 'git submodule status'
   patches = [ ./vtsls-build-patch.patch ];
 
   # Skips manual confirmations during build
@@ -48,35 +47,29 @@ stdenv.mkDerivation (finalAttrs: rec {
 
   buildPhase = ''
     runHook preBuild
-
-    # During build vtsls needs a working git installation. '--global' required
-    # because of submodules.
+    # During build vtsls needs a working git installation.
     git config --global user.name nixbld
     git config --global user.email nixbld@example.com
-
     # This is the sha for submodule packages/service/vscode. Unsure if there's a
     # better way to get this.
     echo "65d85f4cd0b4220fe8bb494898e001c7ad20e59b" > ./packages/service/HEAD
-
     # Requires a git repository during build
     git init packages/service/vscode
-
     # Depends on the @vtsls/language-service workspace
     # '--workspace-concurrency=1' helps debug failing builds.
     pnpm --filter "@vtsls/language-server..." build
-
+    # These trash deterministic builds. During build the whole directory is
+    # copied to another path.
     rm -rf packages/service/vscode/.git
-
+    rm -rf packages/service/src/typescript-language-features/.git
     runHook postBuild
   '';
 
   installPhase = ''
     runHook preInstall
-
     mkdir -p $out/{bin,lib/vtsls-language-server}
     cp -r {packages,node_modules} $out/lib/vtsls-language-server
     ln -s $out/lib/vtsls-language-server/packages/server/bin/vtsls.js $out/bin/vtsls
-
     runHook postInstall
   '';
 
@@ -84,9 +77,7 @@ stdenv.mkDerivation (finalAttrs: rec {
     description = "LSP wrapper for typescript extension of vscode.";
     homepage = "https://github.com/yioneko/vtsls";
     license = licenses.mit;
-    maintainers = with maintainers; [ kuglimon ];
-    mainProgram = "bin/vtsls";
+    mainProgram = "vtsls";
     platforms = platforms.all;
   };
 })
-
