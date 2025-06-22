6f74339a6cc3473927fad4db8815f77ee6beb615
Tatu Argillander <taatelio@gmail.com>
2025-06-22 11:14:31 +0300

    nvim: cleanup comments

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,6 @@
 --
 -- Auto-completion support (nvim-cmp + NeoVim LSP)
 --
--- TODO this is not only about autocompletion
---
 
 --
 -- LSP configuration
@@ -17,13 +15,8 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 -- LSP settings.
 -- This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(client, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is
-  -- possible to define small helper and utility functions so you don't have to
-  -- repeat yourself many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings
-  -- specific for LSP related items. It sets the mode, buffer and description
-  -- for us each time.
+  -- Function that lets us more easily define mappings specific for LSP related
+  -- items. Set mode, buffer and description.
   local nmap = function(keys, func, desc)
     if desc then
       desc = 'LSP: ' .. desc
