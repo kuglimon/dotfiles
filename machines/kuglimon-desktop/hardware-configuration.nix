c0319a8568eaadfc77585bf51813a57959b9f253
Tatu Argillander <taatelio@gmail.com>
2025-10-18 22:50:54 +0300

    style: format hardware-configuration

--- machines/kuglimon-desktop/hardware-configuration.nix
+++ machines/kuglimon-desktop/hardware-configuration.nix
@@ -3,47 +3,78 @@
   lib,
   modulesPath,
   ...
-}: {
+}:
+{
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
-  boot.extraModulePackages = [];
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "ahci"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
     device = "/dev/nvme1n1p1";
     fsType = "btrfs";
-    options = ["subvol=@nixos-root" "noatime" "ssd" "compress=zstd"];
+    options = [
+      "subvol=@nixos-root"
+      "noatime"
+      "ssd"
+      "compress=zstd"
+    ];
   };
 
   fileSystems."/home" = {
     device = "/dev/nvme1n1p1";
     fsType = "btrfs";
-    options = ["subvol=@home" "noatime" "ssd" "compress=zstd"];
+    options = [
+      "subvol=@home"
+      "noatime"
+      "ssd"
+      "compress=zstd"
+    ];
   };
 
   fileSystems."/home/kuglimon/.local/share/Steam" = {
     device = "/dev/nvme1n1p1";
     fsType = "btrfs";
-    options = ["subvol=@steam" "noatime" "ssd" "compress=zstd"];
+    options = [
+      "subvol=@steam"
+      "noatime"
+      "ssd"
+      "compress=zstd"
+    ];
   };
 
   fileSystems."/nix" = {
     device = "/dev/nvme1n1p1";
     fsType = "btrfs";
-    options = ["subvol=@nix-store" "noatime" "ssd" "compress=zstd"];
+    options = [
+      "subvol=@nix-store"
+      "noatime"
+      "ssd"
+      "compress=zstd"
+    ];
   };
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/A836-5A29";
     fsType = "vfat";
-    options = ["fmask=0022" "dmask=0022"];
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
   };
 
-  swapDevices = [];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
