7e52b55ce5509af3491a72e5e274b62fc47fdeb0
Tatu Argillander <taatelio@gmail.com>
2025-11-19 21:31:21 +0200

    desktop: merge fs configuration to host config
    Trying to end up with one configuration file per host that just enables
    a bunch of modules.

--- hosts/kuglimon-desktop/configuration.nix
+++ hosts/kuglimon-desktop/configuration.nix
@@ -39,6 +39,61 @@
   bundles.android.enable = true;
   bundles.users.enable = true;
 
+  fileSystems."/" = {
+    device = "/dev/nvme0n1p1";
+    fsType = "btrfs";
+    options = [
+      "subvol=@nixos-root"
+      "noatime"
+      "ssd"
+      "compress=zstd"
+    ];
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/nvme0n1p1";
+    fsType = "btrfs";
+    options = [
+      "subvol=@home"
+      "noatime"
+      "ssd"
+      "compress=zstd"
+    ];
+  };
+
+  fileSystems."/home/kuglimon/.local/share/Steam" = {
+    device = "/dev/nvme0n1p1";
+    fsType = "btrfs";
+    options = [
+      "subvol=@steam"
+      "noatime"
+      "ssd"
+      "compress=zstd"
+    ];
+  };
+
+  fileSystems."/nix" = {
+    device = "/dev/nvme0n1p1";
+    fsType = "btrfs";
+    options = [
+      "subvol=@nix-store"
+      "noatime"
+      "ssd"
+      "compress=zstd"
+    ];
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/A836-5A29";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
+  };
+
+  swapDevices = [ ];
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
