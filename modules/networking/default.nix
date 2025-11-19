20d17819df7ce6c7a83b4b7eb3578d8e527d941b
Tatu Argillander <taatelio@gmail.com>
2025-11-19 21:27:11 +0200

    net: fix incorrect configuration

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -9,6 +9,13 @@
     "1.1.1.1" # cloudflare
   ];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted
+  # networking (the default) this is the recommended approach. When using
+  # systemd-networkd it's still possible to use this option, but it's
+  # recommended to use it in conjunction with explicit per-interface
+  # declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+
   networking.dhcpcd = {
     enable = true;
 
@@ -20,11 +27,5 @@
     extraConfig = ''
       noarp
     '';
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted
-    # networking (the default) this is the recommended approach. When using
-    # systemd-networkd it's still possible to use this option, but it's
-    # recommended to use it in conjunction with explicit per-interface
-    # declarations with `networking.interfaces.<interface>.useDHCP`.
-    useDHCP = lib.mkDefault true;
   };
 }
