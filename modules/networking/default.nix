7750c4d3501b599188a8c5dc935462ef4b5a4f71
Tatu Argillander <taatelio@gmail.com>
2025-11-19 21:25:53 +0200

    networking: move missing configuration to networking module

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -1,5 +1,6 @@
 # Networking setup
 {
+  lib,
   ...
 }:
 {
@@ -19,5 +20,11 @@
     extraConfig = ''
       noarp
     '';
+    # Enables DHCP on each ethernet and wireless interface. In case of scripted
+    # networking (the default) this is the recommended approach. When using
+    # systemd-networkd it's still possible to use this option, but it's
+    # recommended to use it in conjunction with explicit per-interface
+    # declarations with `networking.interfaces.<interface>.useDHCP`.
+    useDHCP = lib.mkDefault true;
   };
 }
