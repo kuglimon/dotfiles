c55dfc054b8fc5b83f4756db4d7ce90667754f0a
Tatu Argillander <taatelio@gmail.com>
2026-01-10 12:52:10 +0200

    nvim: migrate to new treesitter plugin

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -5,21 +5,19 @@
 -- to do filetype detection, which other plugins like LSP require.
 --
 
-require('nvim-treesitter.configs').setup {
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
+-- Enable tree-sitter highlighting globally
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    local bufnr = args.buf
+    local lang = vim.treesitter.language.get_lang(args.match)
 
-    -- Disable in large lua buffers. These files are killing my old mac.
-    disable = function(lang, bufnr)
-      return lang == "lua" and vim.api.nvim_buf_line_count(bufnr) > 2500
-    end,
-
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
+    -- Check if parser exists for this language
+    local ok = pcall(vim.treesitter.start, bufnr, lang)
+    if not ok then
+      return
+    else
+      -- Disable additional vim regex highlighting
+      vim.bo[bufnr].syntax = ""
+    end
+  end,
+})
