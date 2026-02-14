f29b8310a4a7f36b6c41f7629b33a60405bb5997
Tatu Argillander <taatelio@gmail.com>
2026-02-14 16:33:09 +0200

    nvim: Stop using create_default_mappings in leap
    Seems like this is gone now. The new binding seems nicer as it works on
    the whole window.

--- dotfiles/nvim/lua/plugins/leap.lua
+++ dotfiles/nvim/lua/plugins/leap.lua
@@ -1,6 +1,19 @@
 local leap = require('leap')
 
-leap.create_default_mappings()
-
 -- default mappings have '/' and '?' as labels and those are annoying to type
 leap.labels = 'sfnjklhodweimbuyvrgtaqpcxzSFNJKLHODWEIMBUYVRGTAQPCXZ'
+
+vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+
+-- Highly recommended: define a preview filter to reduce visual noise
+-- and the blinking effect after the first keypress
+-- (see `:h leap.opts.preview`).
+-- For example, skip preview if the first character of the match is
+-- whitespace or is in the middle of an alphabetic word:
+leap.opts.preview = function(ch0, ch1, ch2)
+  return not (
+    ch1:match('%s')
+    or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+  )
+end
